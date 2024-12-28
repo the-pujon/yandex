25dd658b496b6892e05c96968942f79b9776f6ce
Pujon Das Auvi <auvipujondas@gmail.com>
2024-12-28 14:16:47 +0600

    bg updated and task finish

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { HeroBanner } from '@/components/hero-banner'
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-background flex flex-col">
+    <div className="min-h-screen bg-[#F5F4F2] flex flex-col">
       <Header />
       <main className="flex-grow container mx-auto grid grid-cols-1 gap-6 md:grid-cols-[240px_1fr_300px] pt-4">
         <Sidebar />
